7fce5dc4fdfa058d106c31a80f7c7077b7f20bbe
Denes Beck <85183401+denesbeck@users.noreply.github.com>
2022-09-19 09:12:33 +0200

    Update ThemeSwitcher.tsx

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from 'next-themes'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { BsMoon, BsSun } from 'react-icons/bs'
 
 const ThemeSwitcher = () => {
