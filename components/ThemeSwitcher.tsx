98660b2fcf2ffe3da667bdf8dc3b6acd99c1c33e
Denes Beck <85183401+denesbeck@users.noreply.github.com>
2022-09-19 09:11:37 +0200

    Update ThemeSwitcher.tsx

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -22,7 +22,6 @@ const ThemeSwitcher = () => {
     const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark')
     
     return (
-        <div className='flex h-auto items-center'>
             <button
                 className='h-8 w-8 rounded-full p-2 text-gray-200 outline-none outline-offset-2 ring-1 ring-slate-700 transition-all duration-200 ease-in-out hover:bg-slate-700 focus-visible:outline-blue-300'
                 onClick={toggleTheme}
@@ -33,7 +32,6 @@ const ThemeSwitcher = () => {
                     <BsSun className='h-full w-full animate-fadeIn' />
                 )}
             </button>
-        </div>
     )
 }
 
